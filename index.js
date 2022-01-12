231dd93a28946eaf099d29fdde209f55696759ba
Dalton <slattery.da@gmail.com>
2022-01-12 16:08:45 -0600

    commit tree 43601ad4c720a2eeb31f82fb8f736f95b8d1b475

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, str){
+    let newArray = []
+    newArray = array.filter(name => name.toLowerCase() === str.toLowerCase())
+    return newArray
+}
+function fuzzyMatch(array, str){
+    let newArray = []
+    newArray = array.filter(name => name[0] === str[0])
+    return newArray
+}
+
+function matchName(obj, str){
+    //each obj has name and hometown
+    let newObj = {}
+    newObj = obj.filter(person => person.name === str)
+    return newObj
+}
